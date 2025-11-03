12ffc6c92594da0c68976fd17212ea9591b23cb3
GajBorade <141010355+GajBorade@users.noreply.github.com>
2025-11-03 13:46:04 +0100

    Enhance page and query parameter validation for robust error handling

--- validators.py
+++ validators.py
@@ -1,15 +1,19 @@
 # Validate ID Conversion
+
+
 def validate_book_id(book_id):
     try:
         return int(book_id)
     except ValueError:
         return None
 
+
 # Validate data
 def validate_book_data(data):
     if "title" not in data or "author" not in data:
         return False
     return True
+
 
 def validate_query_parameters(query_parameters, valid_keys):
     """
@@ -35,5 +39,62 @@ def validate_query_parameters(query_parameters, valid_keys):
             invalid_keys.append(key)
 
     if invalid_keys:
-        return False, invalid_keys # validation failed
-    return True, None              # all good
+        return False, invalid_keys  # validation failed
+    return True, None  # all good
+
+
+# Validate Page
+def page_validation(page, total_books, limit):
+    """
+    Validates the 'page' query parameter.
+
+    :param page: str or int from request.args
+    :param limit: int (books per page, say 10 per page)
+    :param total_books: int (total number of books in json)
+    :return: int (valid page number) or raises ValueError
+    """
+    if not page:
+        return 1
+
+    try:
+        page = int(page)
+    except ValueError:
+        raise ValueError("Page must be an integer")
+
+    if page < 1:
+        raise ValueError("Page must be >=1 ")
+
+    max_page = total_books // limit
+    if total_books % limit != 0:
+        max_page += 1
+
+    if page > max_page:
+        raise ValueError(f"Page must not be > {max_page= }")
+
+    return page
+
+
+# Validate limit
+def limit_validation(limit, max_limit=None):
+    """
+    Validates the 'limit' query parameter
+
+    :param limit: str or int from request.args
+    :param max_limit: optional maximum allowed limit
+    :return: int (validated limit) or raise ValueError
+    """
+    if max_limit is None:
+        max_limit = 10  # Default maximum limit
+
+    try:
+        limit = int(limit)
+    except ValueError:
+        raise ValueError("limit must be an integer")
+
+    if limit < 1:
+        raise ValueError("limit must be >= 1")
+
+    if limit > max_limit:
+        raise ValueError(f"limit must not exceed {max_limit= }")
+
+    return limit
